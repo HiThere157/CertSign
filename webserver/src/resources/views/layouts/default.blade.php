fd9cd5be2349e39d7957844529b03ba585ed6cff
David <kdavid03@gmx.de>
2022-03-30 13:12:00 +0200

    Update default.blade.php

--- webserver/src/resources/views/layouts/default.blade.php
+++ webserver/src/resources/views/layouts/default.blade.php
@@ -5,7 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
         <title>CertSign</title>
-        <link rel="stylesheet" href={{ asset('css/bootstrap.min.css') }}>
+        <link rel="stylesheet" href="{{ asset('css/bootstrap.min.css') }}">
         <script src="{{ asset('js/jquery.min.js') }}"></script>
 
     </head>
