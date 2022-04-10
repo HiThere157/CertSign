f31a4a1744fb2726902a15a6494f01aa2524dded
David <kdavid03@gmx.de>
2022-04-10 15:03:17 +0200

    Update restore.blade.php

--- resources/views/pages/restore.blade.php
+++ resources/views/pages/restore.blade.php
@@ -19,27 +19,29 @@
         </thead>
         <tbody>
             @foreach($certificates as $certificate)
-                <tr>
-                    <td>{{ $certificate->id }}</td>
-                    <td>
-                        <span>{{ $certificate->name }}</span>
-                        <span>
-                            @if($certificate->daysValid() > 0)
-                                <span class="badge bg-success">Valid</span>
-                            @else
-                                <span class="badge bg-danger">Invalid</span>
-                            @endif
-                        </span>
-                    </td>
-                    <td>{{ $certificate->owner->username }}</td>
-                    <td>[0{{ dechex($certificate->issuer->serial_number) }}] {{ $certificate->issuer->name }}</td>
-                    <td>{{ $certificate->valid_from }}</td>
-                    <td>{{ $certificate->valid_to }} ({{ $certificate->daysValid() }} days)</td>
-                    <td>0{{ dechex($certificate->serial_number) }}</td>
-                    <td class="text-end">
-                        <a class="btn btn-primary" href="{{ route('certificate.restore', $certificate->id) }}">Restore</a>
-                    </td>
-                </tr>
+                @can('owns-cert', $certificate)
+                    <tr>
+                        <td>{{ $certificate->id }}</td>
+                        <td>
+                            <span>{{ $certificate->name }}</span>
+                            <span>
+                                @if($certificate->daysValid() > 0)
+                                    <span class="badge bg-success">Valid</span>
+                                @else
+                                    <span class="badge bg-danger">Invalid</span>
+                                @endif
+                            </span>
+                        </td>
+                        <td>{{ $certificate->owner->username }}</td>
+                        <td>[0{{ dechex($certificate->issuer->serial_number) }}] {{ $certificate->issuer->name }}</td>
+                        <td>{{ $certificate->valid_from }}</td>
+                        <td>{{ $certificate->valid_to }} ({{ $certificate->daysValid() }} days)</td>
+                        <td>0{{ dechex($certificate->serial_number) }}</td>
+                        <td class="text-end">
+                            <a class="btn btn-primary" href="{{ route('certificate.restore', $certificate->id) }}">Restore</a>
+                        </td>
+                    </tr>
+                @endcan
             @endforeach
         </tbody>
     </table>
