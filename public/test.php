b3217fc11407e8986523a3109f7864f14d00270e
David <kdavid03@gmx.de>
2022-06-08 18:02:04 +0200

    Delete test.php

--- public/test.php
+++
@@ -1,9 +0,0 @@
-<?php
-           $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
-           $randstring = '';
-           for ($i = 0; $i < 30; $i++) {
-               $randstring .= $characters[rand(0, strlen($characters)-1)];
-           }
-           echo $randstring;
-        
-?>
